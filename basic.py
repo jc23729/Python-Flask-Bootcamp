d8ba52831c60075b53e896274d05e9158a611c18
Juan Cruz <jc23729@gmail.com>
2021-01-15 09:59:32 -0500

    Dynamic routes

--- basic.py
+++ basic.py
@@ -6,9 +6,24 @@ app = Flask(__name__)
 def index():
   return '<h1>Hello Puppy!</h1>'
 
+## this will bring page another page
 @app.route('/information')
 def info():
     return "<h1>Puppies are cute!</h1>"
+
+##Dynamic routes are routes where you may want a page per user
+# Dyamic routes have a variable passed in, and parmaeter passed in function
+"""
+@app.route('/some_page/<name>')
+def other_page(name):
+    #Later we will see how to use 
+    #this parameter with templates!
+    return 'User: {}'.format(name)
+"""
+@app.route('/puppy/<name>')
+def puppy(name):
+    return "<h1>This a page for {}</h1>".format(name)
+
 
 
 if __name__ == '__main__':
