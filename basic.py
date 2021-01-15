542c98447d146c5855aca06ded6143d04e4fb78e
Juan Cruz <jc23729@gmail.com>
2021-01-15 09:42:32 -0500

    Update

--- basic.py
+++ basic.py
@@ -1,9 +1,10 @@
 from flask import Flask
 app = Flask(__name__)
-
+#one view function which is our main page
 @app.route('/')
 def index():
   return '<h1>Hello Puppy!</h1>'
 
 if __name__ == '__main__':
-  app.run()
+    app.run()
+
