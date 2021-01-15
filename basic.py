e78d577ac61d8380b7f74d1790930fe0b6495296
Juan Cruz <jc23729@gmail.com>
2021-01-15 09:48:16 -0500

    setup virtual folder, froze requirements

--- basic.py
+++ basic.py
@@ -1,9 +1,15 @@
 from flask import Flask
 app = Flask(__name__)
+
 #one view function which is our main page
 @app.route('/')
 def index():
   return '<h1>Hello Puppy!</h1>'
+
+@app.route('/information')
+def info():
+    return "<h1>Puppies are cute!</h1>"
+
 
 if __name__ == '__main__':
     app.run()
